0028703881d9e957138170a586af26c36b67f0ac
youliangdao <youliangdaojing5@gmail.com>
2023-11-05 18:09:56 +0900

    [feat] ユーザー入力フォームを作成

--- src/app/on-boarding/enter-name/page.tsx
+++ src/app/on-boarding/enter-name/page.tsx
@@ -1,19 +1,64 @@
 'use client'
 
-import { Button, Group, Space, Text, Title } from '@mantine/core'
-import { useWindowScroll } from '@mantine/hooks'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Space, Text, Title } from '@mantine/core'
+import { useLocalStorage } from '@mantine/hooks'
 import { nprogress } from '@mantine/nprogress'
 import { useRouter } from 'next/navigation'
+import objectHash from 'object-hash'
 import { useEffect } from 'react'
+import { FormProvider, useForm } from 'react-hook-form'
 
 import { Form } from '@/components'
+import { FormType, schema } from '@/components/Form/schema'
 
 export default function EnterName() {
-  const [scroll, scrollTo] = useWindowScroll()
   const router = useRouter()
   useEffect(() => {
     nprogress.set(60)
   }, [])
+
+  const form = useForm<FormType>({
+    defaultValues: {
+      title: '',
+      description: '',
+      startTime: '',
+      endTime: '',
+    },
+    resolver: zodResolver(schema),
+    mode: 'onChange',
+  })
+
+  const [formLocalData, setFormLocalData] = useLocalStorage<FormType>({
+    key: 'form',
+    defaultValue: {
+      title: '',
+      date: null,
+      description: '',
+      startTime: '',
+      endTime: '',
+    },
+  })
+
+  const handleClick = () => {
+    setFormLocalData(form.getValues())
+    nprogress.set(80)
+    router.push('/on-boarding/preview')
+  }
+
+  // ToDo: ローカルストレージから取得した日付を初期値として設定する
+  useEffect(() => {
+    if (formLocalData) {
+      form.reset({
+        title: formLocalData.title,
+        date: new Date(),
+        description: formLocalData.description,
+        startTime: formLocalData.startTime,
+        endTime: formLocalData.endTime,
+      })
+    }
+  }, [formLocalData, form])
+
   return (
     <>
       <div className="pb-10 text-center">
@@ -27,29 +72,12 @@ export default function EnterName() {
           以下のフォーマットに従って、必要な情報を記入しましょう
         </Text>
       </div>
-      <Form />
-      <Group justify="center" className="mt-10">
-        <Button
-          size="lg"
-          variant="default"
-          onClick={() => {
-            nprogress.set(25)
-            router.push('/on-boarding/pick-template')
-          }}
-        >
-          戻る
-        </Button>
-        <Button
-          size="lg"
-          onClick={() => {
-            nprogress.set(80)
-            scrollTo({ y: 0 })
-            router.push('/on-boarding/preview')
-          }}
-        >
-          次へ
-        </Button>
-      </Group>
+      <FormProvider {...form}>
+        <Form
+          key={objectHash(formLocalData || {})}
+          {...{ handleClick, formLocalData }}
+        />
+      </FormProvider>
     </>
   )
 }
