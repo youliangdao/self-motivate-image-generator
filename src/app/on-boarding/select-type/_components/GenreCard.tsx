5b4b0da8f9fe087467e90efd0e9d3cba06fee247
youliangdao <youliangdaojing5@gmail.com>
2023-11-05 18:10:42 +0900

    [feat] ジャンル選択画面を作成

--- src/app/on-boarding/select-type/_components/GenreCard.tsx
+++ src/app/on-boarding/select-type/_components/GenreCard.tsx
@@ -1,12 +1,30 @@
 import { Card, Center, Image, Text } from '@mantine/core'
 
-import classes from './GenreCard.module.css'
+import { cn } from '@/utils/twMerge'
 
-export function GenreCard({ genre, image }: { genre: string; image: string }) {
+export function GenreCard({
+  genre,
+  image,
+  isSelected,
+  onClick,
+}: {
+  genre: string
+  image: string
+  isSelected: boolean
+  onClick: () => void
+}) {
   return (
-    <Card withBorder radius="md" className={classes.card}>
+    <Card
+      withBorder
+      radius="md"
+      className={cn(
+        'border-gray-2 cursor-pointer hover:bg-gray-100',
+        isSelected && 'border-4 border-gray-900',
+      )}
+      onClick={onClick}
+    >
       <Card.Section>
-        <Image src={image} height={250} fit="cover" />
+        <Image src={image} height={250} fit="cover" alt="画像" />
       </Card.Section>
 
       <Center className="pt-5">
---
+++ src/constants/initialGenreData.ts
@@ -0,0 +1,130 @@
+import business from '/public//business.png'
+import animal from '/public/animal.png'
+import event from '/public/event.png'
+import food from '/public/food.png'
+import game from '/public/game.png'
+import health from '/public/health.png'
+import hobby from '/public/hobby.png'
+import living from '/public/living.png'
+import nature from '/public/nature.png'
+import other from '/public/other.png'
+import programming from '/public/programming.png'
+import shopping from '/public/shopping.png'
+import sports from '/public/sports.png'
+import travel from '/public/travel.png'
+import { GenreData } from '@/app/on-boarding/select-type/page'
+
+export const initialGenreData: GenreData = [
+  {
+    name: '勉強',
+    genre: 'programming',
+    image: programming.src,
+    isSelected: false,
+    ogpImage:
+      'https://youliangdao.s3.ap-northeast-1.amazonaws.com/computer_programmer.png',
+  },
+  {
+    name: '趣味',
+    genre: 'hobby',
+    image: hobby.src,
+    isSelected: false,
+    ogpImage:
+      'https://youliangdao.s3.ap-northeast-1.amazonaws.com/hobby_image.png',
+  },
+  {
+    name: '自然',
+    genre: 'nature',
+    isSelected: false,
+    image: nature.src,
+    ogpImage:
+      'https://youliangdao.s3.ap-northeast-1.amazonaws.com/nature_color.png',
+  },
+  {
+    name: '動物',
+    genre: 'animal',
+    isSelected: false,
+    image: animal.src,
+    ogpImage:
+      'https://youliangdao.s3.ap-northeast-1.amazonaws.com/animal_color.png',
+  },
+  {
+    name: '健康',
+    genre: 'health',
+    isSelected: false,
+    image: health.src,
+    ogpImage:
+      'https://youliangdao.s3.ap-northeast-1.amazonaws.com/helth_color.png',
+  },
+  {
+    name: '食べ物・料理',
+    genre: 'food',
+    isSelected: false,
+    image: food.src,
+    ogpImage:
+      'https://youliangdao.s3.ap-northeast-1.amazonaws.com/food_omoide.png',
+  },
+  {
+    name: 'スポーツ',
+    genre: 'sports',
+    isSelected: false,
+    image: sports.src,
+    ogpImage:
+      'https://youliangdao.s3.ap-northeast-1.amazonaws.com/sports_ultimate.png',
+  },
+  {
+    name: 'ゲーム',
+    genre: 'game',
+    isSelected: false,
+    image: game.src,
+    ogpImage:
+      'https://youliangdao.s3.ap-northeast-1.amazonaws.com/game_controller.png',
+  },
+  {
+    name: 'ショッピング',
+    genre: 'shopping',
+    isSelected: false,
+    image: shopping.src,
+    ogpImage:
+      'https://youliangdao.s3.ap-northeast-1.amazonaws.com/shopping_cart.png',
+  },
+  {
+    name: '旅行',
+    genre: 'travel',
+    isSelected: false,
+    image: travel.src,
+    ogpImage:
+      'https://youliangdao.s3.ap-northeast-1.amazonaws.com/travel_color.png',
+  },
+  {
+    name: '生活',
+    genre: 'living',
+    isSelected: false,
+    image: living.src,
+    ogpImage:
+      'https://youliangdao.s3.ap-northeast-1.amazonaws.com/living_color.png',
+  },
+  {
+    name: 'イベント',
+    genre: 'event',
+    isSelected: false,
+    image: event.src,
+    ogpImage:
+      'https://youliangdao.s3.ap-northeast-1.amazonaws.com/pinata_halloween_bat_girl.png',
+  },
+  {
+    name: '仕事',
+    genre: 'business',
+    isSelected: false,
+    image: business.src,
+    ogpImage:
+      'https://youliangdao.s3.ap-northeast-1.amazonaws.com/businessman_workaholic.png',
+  },
+  {
+    name: 'その他',
+    genre: 'other',
+    isSelected: false,
+    image: other.src,
+    ogpImage:
+      'https://youliangdao.s3.ap-northeast-1.amazonaws.com/setsumeikai_seminar.png',
+  },
+]
