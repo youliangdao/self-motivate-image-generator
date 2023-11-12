070d0779055b968f7f291e364b7a52f2932d3a83
youliangdao <youliangdaojing5@gmail.com>
2023-11-12 18:28:52 +0900

    [fix] CardCarouselコンポーネントを修正

--- src/components/CardCarousel/CardsCarousel.tsx
+++ src/components/CardCarousel/CardsCarousel.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 'use client'
 
 import { Carousel } from '@mantine/carousel'
